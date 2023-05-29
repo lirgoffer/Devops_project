7212264acbb10fce18c6d3302c64a35c837f6a67
Lir <lirg177@gmail.com>
2023-05-29 11:31:09 +0300

    .

--- tests/test1.test.js
+++ tests/test1.test.js
@@ -5,7 +5,7 @@ describe("Test suit 1:", ()=>{
     test("test 1:", async ()=>{
         const res = await request(app).get('/')
         expect(res.statusCode).toEqual(200)
-    })
+    }) 
     test("test 2:", async ()=>{
         const res = await request(app).get('/1234')
         expect(res.statusCode).toEqual(404)
