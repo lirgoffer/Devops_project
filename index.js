6278dbd62e2eb72c5823b577e53cb0b41e46ce40
Lir <lirg177@gmail.com>
2023-05-30 13:29:00 +0300

    fixed index.js

--- index.js
+++ index.js
@@ -5,28 +5,41 @@ app.listen(port, () => (
   console.log('Server started!')
 ));
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    // Sample data
-    const students = [
-      { name: 'John Doe', exam1: 90, exam2: 85, exam3: 95 },
-      { name: 'Jane Smith', exam1: 80, exam2: 92, exam3: 88 },
-      { name: 'Mike Johnson', exam1: 95, exam2: 88, exam3: 93 },
-    ];
+// Sample student data
+const students = [
+    { name: 'John Doe', exam1: 85, exam2: 92, exam3: 78 },
+    { name: 'Jane Smith', exam1: 76, exam2: 88, exam3: 95 },
+    { name: 'Alice Johnson', exam1: 91, exam2: 84, exam3: 89 }
+  ];
   
-    // Get the table body
-    const tableBody = document.querySelector('#gradesTable tbody');
+  // Function to populate the table with student grades
+  function populateTable() {
+    const gradeTable = document.getElementById('grade-table');
   
-    // Populate the table rows with data
     students.forEach((student) => {
-      const tableRow = document.createElement('tr');
-      tableRow.innerHTML = `
-        <td>${student.name}</td>
-        <td>${student.exam1}</td>
-        <td>${student.exam2}</td>
-        <td>${student.exam3}</td>
-      `;
-      tableBody.appendChild(tableRow);
-    });
-  });
+      const row = document.createElement('tr');
   
+      const nameCell = document.createElement('td');
+      nameCell.textContent = student.name;
+      row.appendChild(nameCell);
+  
+      const exam1Cell = document.createElement('td');
+      exam1Cell.textContent = student.exam1;
+      row.appendChild(exam1Cell);
+  
+      const exam2Cell = document.createElement('td');
+      exam2Cell.textContent = student.exam2;
+      row.appendChild(exam2Cell);
+  
+      const exam3Cell = document.createElement('td');
+      exam3Cell.textContent = student.exam3;
+      row.appendChild(exam3Cell);
+  
+      gradeTable.appendChild(row);
+    });
+  }
+  
+  // Call the populateTable function when the DOM content is loaded
+  module.exports = populateTable;
+  
+
