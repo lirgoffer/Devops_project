c737fdf00f16e0462614a9289f56bab7130e32b3
Lir <lirg177@gmail.com>
2023-06-02 12:03:09 +0300

    connected to db

--- index.js
+++ index.js
@@ -1,41 +1,8 @@
 const app = require('./server');
 
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => (
   console.log('Server started!')
 ));
 
-// Sample student data
-const students = [
-    { name: 'John Doe', exam1: 85, exam2: 92, exam3: 78 },
-    { name: 'Jane Smith', exam1: 76, exam2: 88, exam3: 95 },
-    { name: 'Alice Johnson', exam1: 91, exam2: 84, exam3: 89 }
-  ];
-  
-  // Function to populate the table with student grades
-  function populateTable() {
-    const gradeTable = document.getElementById('gradesTable');
-  
-    students.forEach((student) => {
-      const row = document.createElement('tr');
-  
-      const nameCell = document.createElement('td');
-      nameCell.textContent = student.name;
-      row.appendChild(nameCell);
-  
-      const exam1Cell = document.createElement('td');
-      exam1Cell.textContent = student.exam1;
-      row.appendChild(exam1Cell);
-  
-      const exam2Cell = document.createElement('td');
-      exam2Cell.textContent = student.exam2;
-      row.appendChild(exam2Cell);
-  
-      const exam3Cell = document.createElement('td');
-      exam3Cell.textContent = student.exam3;
-      row.appendChild(exam3Cell);
-  
-      gradeTable.appendChild(row);
-    });
-  }
-  
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+
+const mongoose = require("mongoose");
+const database = module.exports = () => {
+    const connectionParams = {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    }
+    try {
+        mongoose.connect('mongodb+srv://lirg177:Lr220520@cluster0.hajlzup.mongodb.net/mongodb?retryWrites=true&w=majority'
+            , connectionParams
+        )
+        console.log("database connected");
+
+
+    } catch (error) {
+        console.log('error')
+        
+    }
+}
+
+database();
 
 app.use(express.static(path.join(__dirname, 'html')));
 
