025f56c7a94d7eca8ed33fa591c69da3131e87ec
Lir <lirg177@gmail.com>
2023-06-04 18:33:36 +0300

    changes index and server

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 const app = require('./server');
 
+const mongoose = require('mongoose');
+
+const gradesRouter = require('./routes/grades_route');
+
+app.use('/grades', gradesRouter);
+
+const database = () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  try {
+    mongoose.connect(
+      'mongodb+srv://lirg177:Lr220520@cluster0.hajlzup.mongodb.net/mongodb?retryWrites=true&w=majority',
+      connectionParams,
+    );
+    console.log('database connected');
+  } catch (error) {
+    console.log('error');
+  }
+};
+
+module.exports = database;
+
+database();
+
+app.post('/grades', (req, res) => {
+  const {
+    name, exam1, exam2, exam3,
+  } = req.body;
+  database.storeRegistration(name, exam1, exam2, exam3);
+  res.sendStatus(200);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => (
   console.log('Server started!')
--- server.js
+++ server.js
@@ -1,52 +1,17 @@
-const express = require('express');
+const express = require('express'); //
 
-const path = require('path');
+const path = require('path'); //
 
-const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); //
 
-const mongoose = require('mongoose');
-
-const gradesRouter = require('./routes/grades_route');
+const app = express(); //
 
 app.use(bodyParser.urlencoded({ extended: true, limit: 'lmb' }));
 app.use(bodyParser.json());
-
-app.use('/grades', gradesRouter);
-
-const database = () => {
-  const connectionParams = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  };
-
-  try {
-    mongoose.connect(
-      'mongodb+srv://lirg177:Lr220520@cluster0.hajlzup.mongodb.net/mongodb?retryWrites=true&w=majority',
-      connectionParams,
-    );
-    console.log('database connected');
-  } catch (error) {
-    console.log('error');
-  }
-};
-
-module.exports = database;
-
-database();
-
-app.post('/grades', (req, res) => {
-  const {
-    name, exam1, exam2, exam3,
-  } = req.body;
-  database.storeRegistration(name, exam1, exam2, exam3);
-  res.sendStatus(200);
-});
-
-app.use(express.static(path.join(__dirname, 'html')));
+app.use(express.static(path.join(__dirname, 'html'))); //
 
 app.get('/', (req, res) => (
-  res.sendFile(path.join(__dirname, 'html', 'page.html'))
+  res.sendFile(path.join(__dirname, 'html', 'page.html'))//
 ));
 
 app.get('/', (req, res) => (
@@ -56,4 +21,4 @@ app.get('/test', (req, res) => (
   res.send('test hello')
 ));
 
-module.exports = app;
+module.exports = app; //
